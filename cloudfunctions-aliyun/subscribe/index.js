c11803b327ac54102fe881570cb709151d3a6804
SunJackson <36870974+SunJackson@users.noreply.github.com>
2021-01-01 15:45:12 +0800

    Update index.js

--- cloudfunctions-aliyun/subscribe/index.js
+++ cloudfunctions-aliyun/subscribe/index.js
@@ -3,18 +3,18 @@ const db = uniCloud.database();
 const response = require('response')
 
 function getFormatDate(ms) {
-    var date = new Date();
+    let date = new Date();
 	date.setTime(date.getTime() + ms);
-    var year = date.getFullYear();
-    var month = date.getMonth() + 1;
-    var strDate = date.getDate();
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let strDate = date.getDate();
     if (month >= 1 && month <= 9) {
         month = '0' + month;
     }
     if (strDate >= 0 && strDate <= 9) {
         strDate = '0' + strDate;
     }
-    var currentdate = year + '-' + month + '-' + strDate;
+    let currentdate = year + '-' + month + '-' + strDate;
     return currentdate;
 }
 
